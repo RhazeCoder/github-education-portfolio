cd27eb8d5675198cecd1fdc5d78900bcda48fa34
RhazeCoder <agcanasjustine1@gmail.com>
2023-12-16 09:59:32 +0800

    Updated Commit

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -4,7 +4,7 @@ const Education = () => {
     return (
         <section className="padding" id="education">
             <h2 style={{ textAlign: "center" }}>Education</h2>
-            <div className="container">
+            <div className="container" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
                 <h3 style={{ flexBasis: "40px" }}>Date Center College of the Philippines</h3>
                 <p className="small">BS Information Technology</p>
             </div>
