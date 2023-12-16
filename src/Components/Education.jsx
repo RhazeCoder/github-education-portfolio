b0b93ea3c0c0d76602b1b7afb52374c129c92277
RhazeCoder <agcanasjustine1@gmail.com>
2023-12-16 09:48:04 +0800

    Updated Commit

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -3,17 +3,25 @@ import React from "react";
 const Education = () => {
     return (
         <section className="padding" id="education">
-        <h2 style={{ textAlign: "center" }}>Education</h2>
-        <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+            <h2 style={{ textAlign: "center" }}>Education</h2>
             <div className="container">
-            <div className="box">
-                <a href="https://www.dcc.edu.ph/" target="_blank" rel="noopener noreferrer">
-                <h3 style={{ flexBasis: "40px" }}>Data Center College of the Philippines</h3>
-                </a>
-                <p className="small">Bachelor of Science in Information Technology</p>
+                <div className="box">
+                    <h3 style={{ flexBasis: "40px" }}>Date Center College of the Philippines</h3>
+                    <p className="small">BS Information Technology</p>
+                </div>
+                <div className="box">
+                    <h3 style={{ flexBasis: "40px" }}>Date Center College of the Philippines</h3>
+                    <p className="small">BS Information Technology</p>
+                </div>
+                <div className="box">
+                    <h3 style={{ flexBasis: "40px" }}>Date Center College of the Philippines</h3>
+                    <p className="small">BS Information Technology</p>
+                </div>
+                <div className="box">
+                    <h3 style={{ flexBasis: "40px" }}>Date Center College of the Philippines</h3>
+                    <p className="small">BS Information Technology</p>
+                </div>
             </div>
-            </div>
-        </div>
         </section>
     );
 }
