10312765972b2512c7f054ca4adcd2e230c7e79b
RhazeCoder <agcanasjustine1@gmail.com>
2023-12-16 09:24:40 +0800

    Updated Commit

---
+++ src/Components/Education.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Education = () => {
+    return (
+        <section className="padding" id="education">
+        <h2 style={{ textAlign: "center" }}>Education</h2>
+        <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+            <div className="container">
+            <div className="box">
+                <a href="https://www.dcc.edu.ph/" target="_blank" rel="noopener noreferrer">
+                <h3 style={{ flexBasis: "40px" }}>Data Center College of the Philippines</h3>
+                </a>
+                <p className="small">Bachelor of Science in Information Technology</p>
+            </div>
+            </div>
+        </div>
+        </section>
+    );
+}
